bd6eab2dfec92f9cee3ac6d19acf99abdd41fd2e
Philip Ross <dev@baseballlover723.com>
2023-05-28 02:14:31 -0700

    added better print to validation check

--- src/twitch/twitch_client.cr
+++ src/twitch/twitch_client.cr
@@ -13,6 +13,7 @@ module Twitch
       validate_url = "https://id.twitch.tv/oauth2/validate"
 
       validate_resp = HTTP::Client.get(validate_url, headers: HTTP::Headers{"Authorization" => "OAuth #{Twitch::Config.instance.token.access_token}"})
+      puts "token is #{validate_resp.success? ? "" : "not "}valid"
       puts "validate_resp: #{validate_resp.inspect}" unless validate_resp.success?
       validate_resp.success?
     end
