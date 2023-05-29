cba835bdf3dc95f1b861c07b90a2d1945d31460d
Philip Ross <dev@baseballlover723.com>
2023-05-28 17:05:32 -0700

    refresh token if invalid on startup. add bailing out if access token still isn't valid after refreshing on startup

--- src/twitch/twitch_client.cr
+++ src/twitch/twitch_client.cr
@@ -1,28 +1,41 @@
 require "http/client"
 
 require "./config"
+require "./config/invalid_config_exception"
 
 module Twitch
   class Client
     getter config : Config
 
     def initialize(username : String)
-      @config = Config.new(username)
-      validate
+      initialize(Config.new(username))
     end
 
     def initialize(username : String, token_directory : String)
-      @config = Config.new(username, token_directory)
-      validate
+      initialize(Config.new(username, token_directory))
+    end
+
+    def initialize(@config : Config)
+      if !validate
+        @config.refresh_token!
+        validate!
+      end
     end
 
     def validate : Bool
-      validate_url = "https://id.twitch.tv/oauth2/validate"
+      get_validate.success?
+    end
 
+    def validate! : Nil
+      validate_resp = get_validate
+      raise InvalidConfigException.new(validate_resp.body) unless validate_resp.success?
+    end
+
+    private def get_validate
+      validate_url = "https://id.twitch.tv/oauth2/validate"
       validate_resp = HTTP::Client.get(validate_url, headers: HTTP::Headers{"Authorization" => "OAuth #{@config.token.access_token}"})
       puts "#{@config.username} token is #{validate_resp.success? ? "" : "not "}valid"
-      puts "validate_resp: #{validate_resp.inspect}" unless validate_resp.success?
-      validate_resp.success?
+      validate_resp
     end
   end
 end
