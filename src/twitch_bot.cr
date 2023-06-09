a63d5eff865aaafe184dae70f8996a6b39da5f78
Philip Ross <dev@baseballlover723.com>
2023-06-08 17:36:18 -0700

    added plumbing for enabled the 2nd channel to follow

--- src/twitch_bot.cr
+++ src/twitch_bot.cr
@@ -13,6 +13,8 @@ Signal::INT.trap do
   exit
 end
 
+enable_2nd = ARGV.shift? == "true"
+
 twitch_client = Twitch::Client.new("baseballlover723")
 # user = twitch_client.get_user("baseballlover723")
 # puts "user: #{user}"
@@ -20,7 +22,10 @@ twitch_client = Twitch::Client.new("baseballlover723")
 # twitch_client.post_create_eventsub("channel.channel_points_custom_reward_redemption.add", "1", {broadcaster_user_id: user.id}, "secret")
 
 irc_server = IRC_Server.new(twitch_client, "baseballlover723")
-irc_server2 = IRC_Server.new(twitch_client, "doomerdinger")
+irc_server2 = uninitialized IRC_Server
+if enable_2nd
+  irc_server2 = IRC_Server.new(twitch_client, "doomerdinger")
+end
 
 # spawn do
 #   sleep 50
@@ -30,18 +35,25 @@ irc_server2 = IRC_Server.new(twitch_client, "doomerdinger")
 puts "starting irc server 1"
 irc_server.start
 puts "started irc server 1"
-puts "starting irc server 2"
-irc_server2.start
-puts "started irc server 2"
+if enable_2nd
+  puts "starting irc server 2"
+  irc_server2.start
+  puts "started irc server 2"
+end
 
 websocket_server = Websocket_Server.new(twitch_client, irc_server)
-websocket_server2 = Websocket_Server.new(twitch_client, irc_server2)
+websocket_server2 = uninitialized Websocket_Server
+if enable_2nd
+  websocket_server2 = Websocket_Server.new(twitch_client, irc_server2)
+end
 puts "starting websocket server 1"
 websocket_server.start
 puts "started websocket server 1"
-puts "starting websocket server 2"
-websocket_server2.start
-puts "started websocket server 2"
+if enable_2nd
+  puts "starting websocket server 2"
+  websocket_server2.start
+  puts "started websocket server 2"
+end
 
 puts "waiting for servers to shutdown later"
 IRC_Server.wait_for_servers_to_disconnect
